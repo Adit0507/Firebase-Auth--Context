bffa9a394f6d7a2ad289a0712cd2ff2ba6213810
Adit0507 <adi05072002@gmail.com>
2022-08-27 20:38:07 +0530

    Done with signin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Account from "./components/Account";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
@@ -10,11 +10,13 @@ function App() {
       <h1 className="text-center text-3xl font-bold">
         Firebase Auth & Context
       </h1>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
