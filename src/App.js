14258ebb5e77e2115c03542af970abd60a4616fd
Adit0507 <adi05072002@gmail.com>
2022-08-27 20:40:46 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Account from "./components/Account";
 import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthContextProvider>
     </div>
